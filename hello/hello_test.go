8e7c5153ff9ce51ec18e055621a8367f0dc0378a
Kyle Shannon <kyle@pobox.com>
2018-11-27 14:58:53 -0700

    quote test output

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,10 +1,11 @@
 package main
 
-import(
+import (
 	"testing"
 )
+
 func TestHello(t *testing.T) {
 	if got, want := hello("kyle"), "Hello, kyle!"; got != want {
-		t.Errorf("hello failed, got: %s, want: %s", got, want)
+		t.Errorf("hello failed, got: '%s', want: '%s'", got, want)
 	}
 }
