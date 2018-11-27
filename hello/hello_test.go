6464af8e6cd95cbdf3e51ba89874b79689e1149f
Kyle Shannon <kyle@pobox.com>
2018-11-27 15:08:27 -0700

    add contrib file

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2018, Boise State University All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 package main
 
 import (
