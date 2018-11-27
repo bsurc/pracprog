afb32fddf1bb4df57719324e930ced0ecde055a1
Kyle Shannon <kyle@pobox.com>
2018-11-27 15:17:53 -0700

    add package doc

---
+++ ebird/doc.go
@@ -0,0 +1,13 @@
+// Copyright (c) 2018, Boise State University All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Package ebird provides tools to import and serve historic ebird data from a
+// local SQLite3 database.
+//
+// It requires data downloaded from https://ebird.org/science/download-ebird-data-products.
+//
+// Endpoints:
+//
+// / -> root
+package ebird
