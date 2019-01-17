f4b7b19d8d432351de231b695284e2e0b6b116c4
Kyle Shannon <kyle@pobox.com>
2019-01-17 13:37:28 -0700

    rm doc comment

--- ebird/doc.go
+++ ebird/doc.go
@@ -2,12 +2,4 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package ebird provides tools to import and serve historic ebird data from a
-// local SQLite3 database.
-//
-// It requires data downloaded from https://ebird.org/science/download-ebird-data-products.
-//
-// Endpoints:
-//
-// / -> root
 package ebird
