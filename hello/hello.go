22ef11d465659f6cb31a72b1b2a0d2401f9938fb
Kyle Shannon <kyle@pobox.com>
2019-01-17 14:54:00 -0700

    make use of const and properly print address

--- hello/hello.go
+++ hello/hello.go
@@ -9,12 +9,14 @@ import (
 	"net/http"
 )
 
+const addr string = ":8080"
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!\n")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("open your browser to http://127.0.0.1:8080/")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("open your browser to http://127.0.0.1" + addr)
+	http.ListenAndServe(addr, nil)
 }
