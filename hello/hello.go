7298b65701246152a32dd4bb19465975a0d1cfe9
Kyle Shannon <kyle@pobox.com>
2019-01-17 12:34:37 -0700

    simplify hello

--- hello/hello.go
+++ hello/hello.go
@@ -10,20 +10,12 @@ import (
 	"net/http"
 )
 
-// hello returns the famous 'Hello, World!' string replacing world with
-// name.  If name is an empty string, and empty string is returned.
-func hello(name string) string {
-	if name == "" {
-		return ""
-	}
-	return fmt.Sprintf("Hello %s!", name)
-}
-
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello, World!\n")
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	fmt.Println("open your browser to http://127.0.0.1:8080/")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
