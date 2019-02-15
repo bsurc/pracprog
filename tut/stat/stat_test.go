364105659feb8fee1e1bc7f7f78bf022a17a8132
Kyle Shannon <kyle@pobox.com>
2019-02-15 08:59:06 -0700

    add empty tests/bench

--- tut/stat/stat_test.go
+++ tut/stat/stat_test.go
@@ -1,1 +1,33 @@
 package main
+
+import "testing"
+
+func TestMean(t *testing.T) {
+}
+
+func TestMax(t *testing.T) {
+}
+
+func TestMin(t *testing.T) {
+}
+
+func TestMode(t *testing.T) {
+}
+
+func TestMedian(t *testing.T) {
+}
+
+func BenchmarkMean(t *testing.T) {
+}
+
+func BenchmarkMax(t *testing.T) {
+}
+
+func BenchmarkMin(t *testing.T) {
+}
+
+func BenchmarkMode(t *testing.T) {
+}
+
+func BenchmarkMedian(t *testing.T) {
+}
