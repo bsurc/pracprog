9957975014af5fe6f04e2bac44a45b20ca6e83a2
Kyle Shannon <kyle@pobox.com>
2019-02-22 14:04:44 -0700

    consolidate handlers, use crawshaw

--- ebird/server/server.go
+++ ebird/server/server.go
@@ -1,42 +1,21 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"strconv"
-	"text/tabwriter"
 	"time"
 
-	_ "github.com/mattn/go-sqlite3"
+	"crawshaw.io/sqlite"
+	"crawshaw.io/sqlite/sqlitex"
 )
 
-func htmlHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hello world!</h1>")
-}
 func obsHandler(w http.ResponseWriter, r *http.Request) {
-	row := db.QueryRow(`
-	SELECT common_name, age_sex, observation_count, locality, longitude, latitude,
-		observation_date, species_comments
-		FROM ebird
-		WHERE county_code='US-ID-037' AND species_comments!=''
-		ORDER BY RANDOM() LIMIT 1`)
-	var obs Obs
-	err := row.Scan(&obs.CommonName, &obs.AgeSex, &obs.ObservationCount,
-		&obs.Locality, &obs.Longitude, &obs.Latitude, &obs.ObservationDate,
-		&obs.SpeciesComments)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(obs)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+
 }
 
 func speciesHandler(w http.ResponseWriter, r *http.Request) {
@@ -55,94 +34,116 @@ func speciesHandler(w http.ResponseWriter, r *http.Request) {
 	_, err = time.Parse("2006-01-02", end)
 	if err != nil || end == "" {
 		http.Error(w, fmt.Sprintf("invalid end date: '%s'", end), http.StatusBadRequest)
-		return
 	}
-
-	rows, err := db.Query(`
-	SELECT common_name, age_sex, observation_count, locality, longitude, latitude,
-		observation_date, time_observations_started, species_comments
-		FROM ebird JOIN taxa ON common_name=primary_common_name
-		WHERE species_code=? AND observation_date>? AND observation_date<?
-		GROUP BY group_identifier
-		ORDER BY observation_date`, spp, start, end)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	var (
-		obs   Obs
-		obss  []Obs
-		count string
-	)
-	for rows.Next() {
-		err := rows.Scan(&obs.CommonName, &obs.AgeSex, &count, &obs.Locality,
-			&obs.Longitude, &obs.Latitude, &obs.ObservationDate,
-			&obs.TimeObservationsStarted, &obs.SpeciesComments)
+	var mbr [4]float64
+	for i, x := range []string{"minx", "maxx", "miny", "maxy"} {
+		v := r.FormValue(x)
+		if v == "" {
+			continue
+		}
+		mbr[i], err = strconv.ParseFloat(v, 64)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("invalid coordinate for %s: %s", x, v), http.StatusBadRequest)
 			return
 		}
-		if count == "X" || count == "" {
-			obs.ObservationCount = -1
-		} else {
-			obs.ObservationCount, _ = strconv.Atoi(count)
-		}
-		obss = append(obss, obs)
 	}
+	c := pool.Get(context.Background())
+	defer pool.Put(c)
+	stmt := c.Prep(`SELECT
+		common_name,
+		age_sex,
+		observation_count,
+		locality,
+		longitude,
+		latitude,
+		observation_date,
+		species_comments
+			FROM ebird JOIN taxa ON common_name=primary_common_name
+		WHERE species_code=? AND observation_date>? AND observation_date<?
+		AND longitude>? AND longitude<? AND latitude>? AND latitude<?`)
+	stmt.BindText(1, spp)
+	stmt.BindText(2, start)
+	stmt.BindText(3, end)
+	stmt.BindFloat(4, mbr[0])
+	stmt.BindFloat(5, mbr[1])
+	stmt.BindFloat(6, mbr[2])
+	stmt.BindFloat(7, mbr[3])
+	type obs struct {
+		CommonName string  `json:"common_name"`
+		AgeSex     string  `json:"age_sex"`
+		Count      int64   `json:"count"`
+		Locality   string  `json:"locality"`
+		Longitude  float64 `json:"longitude"`
+		Latitude   float64 `json:"latitude"`
+		Date       string  `json:"date"`
+		Comments   string  `json:"comments"`
+	}
+	var os []obs
+
+	for {
+		if hasRow, err := stmt.Step(); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		} else if !hasRow {
+			break
+		}
+		os = append(os, obs{
+			stmt.ColumnText(0),
+			stmt.ColumnText(1),
+			stmt.ColumnInt64(2),
+			stmt.ColumnText(3),
+			stmt.ColumnFloat(4),
+			stmt.ColumnFloat(5),
+			stmt.ColumnText(6),
+			stmt.ColumnText(7),
+		})
+	}
+
 	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(obss)
+	err = json.NewEncoder(w).Encode(os)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
-func codeHandler(w http.ResponseWriter, r *http.Request) {
-	tw := &tabwriter.Writer{}
-	tw.Init(w, 0, 8, 0, '\t', 0)
-	fmt.Fprintf(tw, "%s\t%s\t%s\t%s\n", "category", "code", "common", "sci")
-	rows, err := db.Query(`SELECT category, species_code, primary_common_name, scientific_name
-		FROM taxa ORDER BY species_code`)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	var (
-		cat  string
-		code string
-		comm string
-		sci  string
-	)
-	for rows.Next() {
-		err = rows.Scan(&cat, &code, &comm, &sci)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		fmt.Fprintf(tw, "%s\t%s\t%s\t%s\n", cat, code, comm, sci)
-	}
-	tw.Flush()
+func codeHandler(w http.ResponseWriter, r *http.Response) {
+	c := pool.Get(context.Background())
+	defer pool.Put(c)
+	stmt := c.Prep(`SELECT
+		common_name,
+		age_sex,
+		observation_count,
+		locality,
+		longitude,
+		latitude,
+		observation_date,
+		species_comments
+			FROM ebird JOIN taxa ON common_name=primary_common_name
+		WHERE species_code=? AND observation_date>? AND observation_date<?`)
+	_ = stmt
 }
 
 var (
-	db *sql.DB
+	pool   *sqlitex.Pool
+	uptime time.Time
 )
 
-const addr = ":8888"
-
 func main() {
+	flagAddr := flag.String("addr", ":8888", "HTTP service address")
+	flag.Parse()
+
 	var err error
-	db, err = sql.Open("sqlite3", os.Args[1])
+	pool, err = sqlitex.Open("ebird.db", sqlite.SQLITE_OPEN_READONLY, 10)
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	mux := &http.ServeMux{}
-	mux.HandleFunc("/", htmlHandler)
 	mux.HandleFunc("/obs", obsHandler)
 	mux.HandleFunc("/species", speciesHandler)
-	mux.HandleFunc("/codes", codeHandler)
 	go func() {
-		fmt.Println("open your browser to http://127.0.0.1" + addr)
+		fmt.Println("open your browser to http://127.0.0.1" + *flagAddr)
 	}()
-	log.Fatal(http.ListenAndServe(addr, mux))
+	uptime = time.Now()
+	log.Fatal(http.ListenAndServe(*flagAddr, mux))
 }
